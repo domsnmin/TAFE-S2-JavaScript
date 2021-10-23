857397bb7371ffb114f28f63da2d69da9a6b0e57
Dom Son <domsnmin@gmail.com>
2021-10-23 20:59:10 +1100

    update commit

--- 04-shopping-list/js/scripts.js
+++ 04-shopping-list/js/scripts.js
@@ -1,75 +1,57 @@
-// checking the 'cage' checkbox
+// https://www.w3schools.com/js/js_typeof.asp
 
-// document.getElementById("cage").addEventListener("click", function(){
-//     console.log("Checkbox was clicked.");
+var string = "I'm a string"; // this is a string.
+var num = 6; // this is a number, technically a integer.
+var checkbox = true; // this is a boolean (true/false).
+
+// var = let = const >> with some differences
+
+const shopping_list = ["Bread", "Milk", "Eggs", "Coffee", "Chocolate", "Tea", "Ice Cream"];
+
+// console.log(shopping_list);
+// console.log(shopping_list[2]);
+// console.log(shopping_list.length); // Length of an array
+
+// for (let i = 0; i < 6; i++) {
+//     console.log(shopping_list[i]);
+// }
+
+
+// Loop through an Array
+
+console.log("...");
+
+for (let i = 0; i < shopping_list.length; i++) {
+    console.log(shopping_list[i]);
+}
+
+// document.getElementById('print').addEventListener('click', function(){
+//     let compiled_list = "";
+//     for (let i = 0; i < shopping_list.length; i++) {
+//         compiled_list += shopping_list[i];
+//         // it is same as,
+//         // compiled_list = compiled_list + shopping_list[i];
+//     }
+
+//     document.getElementById('shopping-printout').innerHTML = compiled_list;
 // });
 
-// document.getElementById("cage").addEventListener("click", function(){
-//     console.log(this);
-// });
 
-document.getElementById("cage").addEventListener("click", function(){
-    console.log("Checkbox is: " + this.checked);
-    // 'this' is referring to the selected "cage" checkbox
-    // var checked = document.getElementById("cage").checked;
-    // console.log(checked);
-});
 
-document.getElementById("vet").addEventListener("click", function(){
-    console.log("Vet button pushed.");
-    // check the "cage checkbox
-    var cage_status = document.getElementById("cage").checked;
-    console.log(cage_status);
+// Above can be re-written, in order to make it a list...!
 
-    // vax cheetah message
-    var email_paragraph = document.getElementById("email"); // Line-1
-
-    // this line has moved into if-Statement
-    // email_paragraph.innerHTML = 'Now is good time to vax the cheetah.'; 
-    // Line-2
-    
-    // if "cage" is true,
-    if(cage_status == true) {
-        console.log("Cheetah is in the cage.");
-        email_paragraph.innerHTML = "Now is good time to vax the cheetah.";
-    } else {
-        console.log("The cage is not locked..!");
-        email_paragraph.innerHTML = "Cage is open. Run!!!";
+document.getElementById('print').addEventListener('click', function(){
+    let compiled_list = "<ul>";
+    for (let i = 0; i < shopping_list.length; i++) {
+        compiled_list += "<li>" + shopping_list[i] + "</li>";
+        // it is same as,
+        // compiled_list = compiled_list + shopping_list[i];
     }
+    // compiled_list += "</ul>"; // to close the "ul"
+    // it works without "compiled_list += "</ul>";" ???
 
-    // change email paragraph
-
-    // if "cage" is false
-    // change email paragraph to something else
+    document.getElementById('shopping-printout').innerHTML = compiled_list;
 });
 
 
-
-
-// document.getElementById("cage").addEventListener("click", function(){
-//     var checked = document.getElementById("cage").checked;
-//     console.log(checked);
-// });
-
-// or, you could,..
-// document.querySelector(#cage)
-
-// 1st, the "respond" for EventListener can be a function, in a form of,..
-// document.getElementById("cage").addEventListener("click", fuction()) {
-//     // Here goes the function from above
-// }
-
-// 2nd way of assigning a fuction is..
-// this one good, when you have many functions to attatch..!
-// document.getElementById("cage").addEventListener("click", doStuff());
-
-// fuction doStuff() {
-//     // function goes here..
-// }
-
-// Interesting ...!
-
-// document.getElementById("cage").addEventListener("click", function(){
-//     console.log(this);
-// });
 
